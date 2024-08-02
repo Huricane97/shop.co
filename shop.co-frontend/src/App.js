3ee850c75e66ce5365d16023b6db973f9532b9bf
Muhammad Abdullah <ab97here@gmail.com>
2024-08-02 19:41:46 +0500

    remove

--- shop.co-frontend/src/App.js
+++ shop.co-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/Home/HomePage';
 import Shop from './pages/Shop/Shop';
@@ -8,23 +8,31 @@ import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 
-
 function App() {
+  const location = useLocation();
+  const showFooter = !['/Login', '/Signup'].includes(location.pathname);
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/ProductDetail" element={<ProductDetail />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+      {showFooter && <Footer />}
+    </div>
+  );
+}
+
+function AppWrapper() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Shop" element={<Shop />} />
-          <Route path="/ProductDetail" element={<ProductDetail />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-        </Routes>
-        <Footer/>
-      </div>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
